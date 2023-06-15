6fd0ae9a4f0926c35f2eab782e8bba661d32d995
mitchwu <benq7879@gmail.com>
2023-06-15 12:58:43 +0800

    feat: add styled CheckList

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,29 @@
 import type { ReactElement } from 'react'
 import Head from '../components/Head'
 import { TextField } from '@mui/material'
+import CheckList from '../components/CheckList'
 
 export default function HomePage(): ReactElement {
+	const items = [
+		{
+			// eslint-disable-next-line @typescript-eslint/no-magic-numbers
+			key: 1,
+			checked: true,
+			label: 'Item 1'
+		},
+		{
+			// eslint-disable-next-line @typescript-eslint/no-magic-numbers
+			key: 2,
+			checked: false,
+			label: 'Item 2'
+		},
+		{
+			// eslint-disable-next-line @typescript-eslint/no-magic-numbers
+			key: 3,
+			checked: true,
+			label: 'Item 3'
+		}
+	]
 	return (
 		<>
 			<Head title='AHA Front-End Assignment Exam 1' />
@@ -12,6 +33,7 @@ export default function HomePage(): ReactElement {
 				</div>
 				<div className='mt-10 flex w-1/2 justify-center'>
 					<TextField label='Password' />
+					<CheckList items={items} />
 				</div>
 			</div>
 		</>
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,8 +9,29 @@ const config = {
 			sans: ['Ubuntu', ...defaultConfig.theme.fontFamily.sans]
 		},
 		extend: {
+			/**
+			 * XXX: follow the guide to pick the colors: https://tailwindcss.com/docs/customizing-colors#using-custom-colors,
+			 * instead of write arbitrary colors
+			 * (because we don't have a full understand of the color palettes in our design system, so we use arbitrary colors now)
+			 */
+
 			colors: {
-				greyscaleBgDark: '#1B1B1B'
+				primary: {
+					light: '#00D1FF'
+				},
+				greyscaleBgDark: '#1B1B1B',
+				greyscale: {
+					700: '#565656',
+					800: '#242424'
+				}
+			},
+			letterSpacing: {
+				tighter: '-.05em',
+				tight: '-.025em',
+				normal: '0',
+				wide: '.017em',
+				wider: '.05em',
+				widest: '.1em'
 			}
 		}
 	},
